feecb3e916cc3a41defba558df2732aa206db85e
Wyatt Johnson <me@wyattjoh.ca>
2020-04-15 18:29:44 -0600

    fix: updated indent

--- src/template.ts
+++ src/template.ts
@@ -6,34 +6,35 @@ const env = nunjucks.configure({ trimBlocks: true, lstripBlocks: true });
 // Define the template source.
 const src = `
 {% for type in definitions %}
-  {{ indent }} Parameters
+{{ indent }} Parameters
 
-  {% for propertyName, property in type.definition.properties | dictsort %}
-  {{ indent }}# \`{{ propertyName }}\` _({{ property.type }}{% if property.enum %}, enum{% endif %})_
+{% for propertyName, property in type.definition.properties | dictsort %}
+{{ indent }}# \`{{ propertyName }}\` _({{ property.type }}{% if property.enum %}, enum{% endif %})_
 
-  {{ property.description | replace("\\n", " ") }}
+{{ property.description | replace("\\n", " ") }}
 
-  {% if property.enum -%}
-  Possible enum values for \`{{ propertyName }}\`:
+{% if property.enum -%}
+Possible enum values for \`{{ propertyName }}\`:
 
-  {% for value in property.enum -%}
+{% for value in property.enum -%}
+- \`{{ value }}\`
+{% endfor %}
+{%- endif %}
+
+{%- if property.properties %}
+{{ indent }}## Attributes
+
+{% for subPropertyName, subProperty in property.properties | dictsort %}
+- \`{{ propertyName }}.{{ subPropertyName }}\` _({{ subProperty.type }}{% if subProperty.enum %}, enum{% endif %})_ - {{ subProperty.description | replace("\\n", " ") }}{% if subProperty.enum %} Possible enum values:
+{% for value in subProperty.enum %}
   - \`{{ value }}\`
-  {% endfor %}
-  {%- endif %}
+{% endfor %}{% endif %}
 
-  {%- if property.properties %}
-  {{ indent }}## Attributes
+{% endfor %}
+{% endif %}
 
-  {% for subPropertyName, subProperty in property.properties | dictsort -%}
-  - \`{{ propertyName }}.{{ subPropertyName }}\` _({{ subProperty.type }}{% if subProperty.enum %}, enum{% endif %})_ - {{ subProperty.description | replace("\\n", " ") }}{% if subProperty.enum %} Possible enum values:
-  {%- for value in subProperty.enum %}
-    - \`{{ value }}\`
-  {%- endfor -%}{%- endif %}
-  {% endfor %}
-  {% endif %}
-
-  {%- endfor %}
-{%- endfor %}
+{% endfor %}
+{% endfor %}
 `;
 
 // Compile the template.
